039abbb4e31261cb4fafb87bb4ae6007b83a1932
ipettrov <ilijapetrov21@gmail.com>
2020-01-22 15:02:09 +0100

    Add project files.

---
+++ E_Dnevnik/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace E_Dnevnik.Models
+{
+    public class User
+    {
+        public User() { }
+        public User(string name, string lastName, string email, string password, bool isTeacher)
+        {
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            IsTeacher = isTeacher;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Your name is required")]
+        [StringLength(160)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Your lst name is required")]
+        [StringLength(160)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Teacher status is required")]
+        public bool IsTeacher { get; set; }
+
+        [Required(ErrorMessage = "Your email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Minimum 8 characters required")]
+        public string Password { get; set; }
+
+      
+    }
+}
---
+++ E_Dnevnik/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Dnevnik.Startup))]
+namespace E_Dnevnik
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
